e29bdc9d531f8bee0cc9b59d62c29077f1e6be99
Devin Droddy <devin.droddy@gmail.com>
2024-01-17 16:27:03 -0500

    use rename and tag macros to make config schema match NohBoard's

--- src/config.rs
+++ src/config.rs
@@ -3,13 +3,18 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Default, Debug)]
 pub struct Config {
+    #[serde(rename = "Version")]
     pub version: String,
+    #[serde(rename = "Width")]
     pub width: u32,
+    #[serde(rename = "Height")]
     pub height: u32,
+    #[serde(rename = "Elements")]
     pub elements: Vec<BoardElement>,
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(tag = "__type")]
 pub enum BoardElement {
     KeyboardKey(KeyboardKeyDefinition),
     MouseKey(MouseKeyDefinition),
@@ -19,43 +24,65 @@ pub enum BoardElement {
 
 #[derive(Deserialize, Debug)]
 pub struct KeyboardKeyDefinition {
+    #[serde(rename = "Id")]
     pub id: u32,
+    #[serde(rename = "Boundaries")]
     pub boundaries: Vec<SerializablePoint>,
+    #[serde(rename = "TextPosition")]
     pub text_position: SerializablePoint,
+    #[serde(rename = "KeyCodes")]
     pub keycodes: Vec<u32>,
+    #[serde(rename = "Text")]
     pub text: String,
+    #[serde(rename = "ShiftText")]
     pub shift_text: String,
+    #[serde(rename = "ChangeOnCaps")]
     pub change_on_caps: bool,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct MouseKeyDefinition {
+    #[serde(rename = "Id")]
     pub id: u32,
+    #[serde(rename = "Boundaries")]
     pub boundaries: Vec<SerializablePoint>,
+    #[serde(rename = "TextPosition")]
     pub text_position: SerializablePoint,
+    #[serde(rename = "KeyCodes")]
     pub keycodes: Vec<u32>,
+    #[serde(rename = "Text")]
     pub text: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct MouseScrollDefinition {
+    #[serde(rename = "Id")]
     pub id: u32,
+    #[serde(rename = "Boundaries")]
     pub boundaries: Vec<SerializablePoint>,
+    #[serde(rename = "TextPosition")]
     pub text_position: SerializablePoint,
+    #[serde(rename = "KeyCodes")]
     pub keycodes: Vec<u32>,
+    #[serde(rename = "Text")]
     pub text: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct MouseSpeedIndicatorDefinition {
+    #[serde(rename = "Id")]
     pub id: u32,
+    #[serde(rename = "Location")]
     pub location: SerializablePoint,
+    #[serde(rename = "Radius")]
     pub radius: u32,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SerializablePoint {
+    #[serde(rename = "X")]
     pub x: u32,
+    #[serde(rename = "Y")]
     pub y: u32,
 }
 
