e7bc7623d35056860e2005927a31eb115346a0a0
Devin Droddy <devin.droddy@gmail.com>
2024-03-22 19:14:55 -0700

    automatically create example keyboard files on first install

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ mod ui;
 use color_eyre::eyre::Result;
 use iced::{multi_window::Application, window};
 use nuhxboard::*;
-use std::{fs::File, io::prelude::*};
+use std::{
+    fs::{self, File},
+    io::{self, prelude::*},
+};
 use types::settings::Settings;
 
 static IMAGE: &[u8] = include_bytes!("../NuhxBoard.png");
@@ -21,7 +24,7 @@ fn main() -> Result<()> {
     let nuhxboard_path = home::home_dir().unwrap().join(".local/share/NuhxBoard");
 
     if !nuhxboard_path.exists() {
-        std::fs::create_dir_all(nuhxboard_path.clone())?;
+        fs::create_dir_all(nuhxboard_path.clone())?;
     }
 
     match std::env::consts::OS {
@@ -64,7 +67,62 @@ fn main() -> Result<()> {
         settings.write_all(serde_json::to_string_pretty(&Settings::default())?.as_bytes())?;
     }
 
-    let settings_file = File::open(nuhxboard_path)?;
+    if !nuhxboard_path.join("keyboards").exists() {
+        fs::create_dir_all(nuhxboard_path.join("keyboards"))?;
+    } else if !nuhxboard_path.join("keyboards").is_dir() {
+        fs::remove_file(nuhxboard_path.join("keyboards"))?;
+        fs::create_dir_all(nuhxboard_path.join("keyboards"))?;
+    }
+
+    if fs::read_dir(nuhxboard_path.join("keyboards"))?.count() == 0 {
+        let res = reqwest::blocking::get(
+            "https://raw.githubusercontent.com/justdeeevin/nuhxboard/main/keyboards.zip",
+        )?;
+
+        let mut keyboards_file = File::options()
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(nuhxboard_path.join("keyboards.zip"))?;
+
+        keyboards_file.write_all(&res.bytes()?)?;
+
+        let mut keyboards_archive = zip::ZipArchive::new(keyboards_file).unwrap();
+
+        for i in 0..keyboards_archive.len() {
+            let mut file = keyboards_archive.by_index(i).unwrap();
+            let outpath = match file.enclosed_name() {
+                Some(path) => nuhxboard_path.join("keyboards").join(path),
+                None => continue,
+            };
+
+            if (*file.name()).ends_with('/') {
+                fs::create_dir_all(&outpath).unwrap();
+            } else {
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p).unwrap();
+                    }
+                }
+                let mut outfile = File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
+
+            // Get and Set permissions
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::PermissionsExt;
+
+                if let Some(mode) = keyboards_file.unix_mode() {
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                }
+            }
+        }
+
+        fs::remove_file(nuhxboard_path.join("keyboards.zip"))?;
+    }
+
+    let settings_file = File::open(nuhxboard_path.join("NuhxBoard.json"))?;
 
     let settings: Settings = serde_json::from_reader(settings_file)?;
 
