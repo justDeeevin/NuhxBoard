21818f0575c395d67886da521124b69fce90a2c6
Devin Droddy <devin.droddy@gmail.com>
2024-08-25 21:11:56 -0400

    reformat flake with alejandra

--- flake.nix
+++ flake.nix
@@ -23,27 +23,36 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
+    advisory-db,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (pkgs) lib;
+      inherit (pkgs) lib;
 
-        craneLib = crane.mkLib pkgs;
-        iconFilter = path: (builtins.match ".*NuhxBoard.png$" path) != null;
-        iconOrCargo = path: type: (iconFilter path) || (craneLib.filterCargoSources path type);
-        src = lib.cleanSourceWith {
-          src = ./.;
-          filter = iconOrCargo;
-          name = "source";
-        };
+      craneLib = crane.mkLib pkgs;
+      # iconFilter = path: (builtins.match ".*NuhxBoard.png$" path) != null;
+      # iconOrCargo = path: type: (iconFilter path) || (craneLib.filterCargoSources path type);
+      # src = lib.cleanSourceWith {
+      #   src = ./.;
+      #   filter = iconOrCargo;
+      #   name = "source";
+      # };
+      src = ./.;
 
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
 
-          buildInputs = with pkgs; [
+        buildInputs = with pkgs;
+          [
             expat
             fontconfig
             freetype
@@ -59,109 +68,117 @@
             wayland
             libxkbcommon
             libevdev
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
           ];
 
-          nativeBuildInputs = with pkgs; [ pkg-config ];
+        nativeBuildInputs = with pkgs; [pkg-config];
 
-          desktopItem = (pkgs.makeDesktopItem {
-            name = "NuhxBoard";
-            desktopName = "NuhxBoard";
-            comment = "Cross-platform input visualizer";
-            icon = "NuhxBoard.png";
-            exec = "nuhxboard";
-            terminal = false;
-            keywords = [ "Keyboard" ];
-            startupWMClass = "NuhxBoard";
-          });
-
-          postInstall = "install -Dm644 $src/NuhxBoard.png $out/share/icons/hicolor/128x128/apps/NuhxBoard.png";
+        desktopItem = pkgs.makeDesktopItem {
+          name = "NuhxBoard";
+          desktopName = "NuhxBoard";
+          comment = "Cross-platform input visualizer";
+          icon = "NuhxBoard.png";
+          exec = "nuhxboard";
+          terminal = false;
+          keywords = ["Keyboard"];
+          startupWMClass = "NuhxBoard";
         };
 
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
+        postInstall = "install -Dm644 $src/NuhxBoard.png $out/share/icons/hicolor/128x128/apps/NuhxBoard.png";
+      };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      craneLibLLvmTools =
+        craneLib.overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        nuhxboard = craneLib.buildPackage (commonArgs // {
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      nuhxboard = craneLib.buildPackage (commonArgs
+        // {
           inherit cargoArtifacts;
         });
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          nuhxboard = nuhxboard;
+    in {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        nuhxboard = nuhxboard;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          nuhxboard-clippy = craneLib.cargoClippy (commonArgs // {
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        nuhxboard-clippy = craneLib.cargoClippy (commonArgs
+          // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           });
 
-          nuhxboard-doc = craneLib.cargoDoc (commonArgs // {
+        nuhxboard-doc = craneLib.cargoDoc (commonArgs
+          // {
             inherit cargoArtifacts;
           });
 
-          # Check formatting
-          nuhxboard-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
+        # Check formatting
+        nuhxboard-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
 
-          # Audit dependencies
-          nuhxboard-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
+        # Audit dependencies
+        nuhxboard-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
 
-          # Audit licenses
-          nuhxboard-deny = craneLib.cargoDeny {
-            inherit src;
-          };
+        # Audit licenses
+        nuhxboard-deny = craneLib.cargoDeny {
+          inherit src;
+        };
 
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `nuhxboard` if you do not want
-          # the tests to run twice
-          nuhxboard-nextest = craneLib.cargoNextest (commonArgs // {
+        # Run tests with cargo-nextest
+        # Consider setting `doCheck = false` on `nuhxboard` if you do not want
+        # the tests to run twice
+        nuhxboard-nextest = craneLib.cargoNextest (commonArgs
+          // {
             inherit cargoArtifacts;
             partitions = 1;
             partitionType = "count";
           });
-        };
+      };
 
-        packages = {
+      packages =
+        {
           default = nuhxboard;
-        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          nuhxboard-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
+        }
+        // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          nuhxboard-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
         };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = nuhxboard;
-        };
+      apps.default = flake-utils.lib.mkApp {
+        drv = nuhxboard;
+      };
 
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
 
-          packages = with pkgs; [
-            cargo-dist
-          ];
+        packages = with pkgs; [
+          cargo-dist
+        ];
 
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath commonArgs.buildInputs}";
-        };
-      });
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath commonArgs.buildInputs}";
+      };
+    });
 }
