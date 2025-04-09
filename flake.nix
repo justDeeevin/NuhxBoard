463fa3c44231a095c403131b8db15c031f3cf4b4
Devin Droddy <devin.droddy@gmail.com>
2025-04-09 08:49:40 -0400

    use rust-overlay instead of fenix + rustup toolchain file

--- flake.nix
+++ flake.nix
@@ -6,27 +6,36 @@
 
     crane.url = "github:ipetkov/crane";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-analyzer-src.follows = "";
-    };
-
     flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
 
         inherit (pkgs) lib;
 
-        craneLib = crane.mkLib pkgs;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
         iconFilter = path: (builtins.match ".*NuhxBoard.png$" path) != null;
-        iconOrCargo = path: type:
-          (iconFilter path) || (craneLib.filterCargoSources path type);
+        iconOrCargo = path: type: (iconFilter path) || (craneLib.filterCargoSources path type);
         src = lib.cleanSourceWith {
           src = ./.;
           filter = iconOrCargo;
@@ -36,7 +45,8 @@
           inherit src;
           strictDeps = true;
 
-          buildInputs = with pkgs;
+          buildInputs =
+            with pkgs;
             [
               expat
               fontconfig
@@ -53,7 +63,8 @@
               wayland
               libxkbcommon
               libevdev
-            ] ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
 
           nativeBuildInputs = with pkgs; [
             copyDesktopItems
@@ -62,43 +73,39 @@
           ];
         };
 
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
-
         # Build *just* the cargo dependencies, so we can reuse
         # all of that work (e.g. via cachix) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
         # Build the actual crate itself, reusing the dependency
         # artifacts from above.
-        nuhxboard = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        } // {
-          desktopItems = [
-            (pkgs.makeDesktopItem {
-              name = "NuhxBoard";
-              desktopName = "NuhxBoard";
-              comment = "Cross-platform input visualizer";
-              icon = "NuhxBoard";
-              exec = "nuhxboard";
-              terminal = false;
-              keywords = [ "Keyboard" ];
-              startupWMClass = "NuhxBoard";
-            })
-          ];
+        nuhxboard = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+          // {
+            desktopItems = [
+              (pkgs.makeDesktopItem {
+                name = "NuhxBoard";
+                desktopName = "NuhxBoard";
+                comment = "Cross-platform input visualizer";
+                icon = "NuhxBoard";
+                exec = "nuhxboard";
+                terminal = false;
+                keywords = [ "Keyboard" ];
+                startupWMClass = "NuhxBoard";
+              })
+            ];
 
-          postInstall = ''
-            install -Dm644 ${src}/NuhxBoard.png $out/share/icons/hicolor/128x128/apps/NuhxBoard.png
-            wrapProgram $out/bin/nuhxboard --prefix LD_LIBRARY_PATH : "${
-              pkgs.lib.makeLibraryPath commonArgs.buildInputs
-            }"
-          '';
-        });
-      in {
+            postInstall = ''
+              install -Dm644 ${src}/NuhxBoard.png $out/share/icons/hicolor/128x128/apps/NuhxBoard.png
+              wrapProgram $out/bin/nuhxboard --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath commonArgs.buildInputs}"
+            '';
+          }
+        );
+      in
+      {
         checks = {
           # Build the crate as part of `nix flake check` for convenience
           nuhxboard = nuhxboard;
@@ -109,13 +116,15 @@
           # Note that this is done as a separate derivation so that
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
-          nuhxboard-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+          nuhxboard-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
 
-          nuhxboard-doc =
-            craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
+          nuhxboard-doc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
 
           # Check formatting
           nuhxboard-fmt = craneLib.cargoFmt { inherit src; };
@@ -123,18 +132,18 @@
           # Run tests with cargo-nextest
           # Consider setting `doCheck = false` on `nuhxboard` if you do not want
           # the tests to run twice
-          nuhxboard-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
+          nuhxboard-nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            }
+          );
         };
 
         packages = {
           default = nuhxboard;
-        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          nuhxboard-llvm-coverage = craneLibLLvmTools.cargoLlvmCov
-            (commonArgs // { inherit cargoArtifacts; });
         };
 
         apps.default = flake-utils.lib.mkApp { drv = nuhxboard; };
@@ -143,10 +152,13 @@
           # Inherit inputs from checks.
           checks = self.checks.${system};
 
-          packages = with pkgs; [ cargo-dist cargo-release ];
+          packages = with pkgs; [
+            cargo-dist
+            cargo-release
+          ];
 
-          LD_LIBRARY_PATH =
-            "${pkgs.lib.makeLibraryPath commonArgs.buildInputs}";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath commonArgs.buildInputs}";
         };
-      });
+      }
+    );
 }
