32ec03c2a494efb9357eeb0bc65cd2071f997d6b
Devin Droddy <devin.droddy@gmail.com>
2025-07-09 18:16:41 -0400

    some better error messages in `fn main`

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::{
 };
 
 use clap::Parser;
-use color_eyre::eyre::ContextCompat;
+use color_eyre::eyre::{eyre, Context};
 use nuhxboard::*;
 use tracing::{debug, debug_span, info, Level};
 use tracing_subscriber::{filter, prelude::*};
@@ -35,33 +35,46 @@ fn main() -> color_eyre::Result<()> {
         tracing_subscriber::fmt::init();
     }
 
-    let config_path = KEYBOARDS_PATH.parent().wrap_err("Config exists in root?")?;
+    let config_path = KEYBOARDS_PATH
+        .parent()
+        .ok_or_else(|| eyre!("Config lives at root?"))?;
 
     if !KEYBOARDS_PATH.exists() {
-        fs::create_dir_all(&*KEYBOARDS_PATH)?;
+        fs::create_dir_all(&*KEYBOARDS_PATH).context("Failed to create config directory")?;
     } else if !KEYBOARDS_PATH.is_dir() {
-        fs::remove_file(&*KEYBOARDS_PATH)?;
-        fs::create_dir_all(&*KEYBOARDS_PATH)?;
+        info!("Config directory exists but is not a directory. Removing and recreating");
+        fs::remove_file(&*KEYBOARDS_PATH).context("Failed to remove file at config path")?;
+        fs::create_dir_all(&*KEYBOARDS_PATH).context("Failed to create config directory")?;
     }
 
     if fs::read_dir(&*KEYBOARDS_PATH)?.count() == 0 {
         info!("Downloading sample keyboards");
         let res = reqwest::blocking::get(
             "https://raw.githubusercontent.com/justdeeevin/nuhxboard/main/keyboards.zip",
-        )?;
+        )
+        .context("Failed to download sample keyboards")?;
 
-        let mut keyboards_file = File::create_new(config_path.join("keyboards.zip"))?;
+        let mut keyboards_file = File::create_new(config_path.join("keyboards.zip"))
+            .context("Failed to create keyboards.zip")?;
 
-        keyboards_file.write_all(&res.bytes()?)?;
+        keyboards_file
+            .write_all(
+                &res.bytes()
+                    .context("Failed to get bytes for keyboards.zip")?,
+            )
+            .context("Failed to write keyboards.zip")?;
 
-        let mut keyboards_archive = zip::ZipArchive::new(keyboards_file).unwrap();
+        let mut keyboards_archive =
+            zip::ZipArchive::new(keyboards_file).context("Failed to load keyboards.zip")?;
 
         info!("Extracting sample keyboards");
         let span = debug_span!("unzip");
         let _guard = span.enter();
         let len = keyboards_archive.len();
         for i in 1..=len {
-            let mut file = keyboards_archive.by_index(i - 1).unwrap();
+            let mut file = keyboards_archive
+                .by_index(i - 1)
+                .with_context(|| format!("Failed to get file #{i} from zip"))?;
             let outpath = match file.enclosed_name() {
                 Some(path) => KEYBOARDS_PATH.join(path),
                 None => continue,
@@ -69,15 +82,19 @@ fn main() -> color_eyre::Result<()> {
             debug!("{} ({i}/{len})", outpath.display());
 
             if (*file.name()).ends_with('/') {
-                fs::create_dir_all(&outpath).unwrap();
+                fs::create_dir_all(&outpath)
+                    .with_context(|| format!("Failed to create directory {outpath:?}"))?;
             } else {
                 if let Some(p) = outpath.parent() {
                     if !p.exists() {
-                        fs::create_dir_all(p).unwrap();
+                        fs::create_dir_all(p)
+                            .with_context(|| format!("Failed to create directory {p:?}"))?;
                     }
                 }
-                let mut outfile = File::create(&outpath).unwrap();
-                io::copy(&mut file, &mut outfile).unwrap();
+                let mut outfile = File::create(&outpath)
+                    .with_context(|| format!("Failed to create file {outpath:?}"))?;
+                io::copy(&mut file, &mut outfile)
+                    .with_context(|| format!("Failed to populate file {outpath:?} from zip"))?;
             }
 
             // Get and Set permissions
@@ -86,18 +103,20 @@ fn main() -> color_eyre::Result<()> {
                 use std::os::unix::fs::PermissionsExt;
 
                 if let Some(mode) = file.unix_mode() {
-                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))
+                        .with_context(|| format!("Failed to set permissions on {outpath:?}"))?;
                 }
             }
         }
 
-        fs::remove_file(config_path.join("keyboards.zip"))?;
+        fs::remove_file(config_path.join("keyboards.zip"))
+            .context("Failed to remove keyboards.zip")?;
     }
 
     let global_path = KEYBOARDS_PATH.join("global");
 
     if !global_path.exists() {
-        fs::create_dir_all(&global_path)?;
+        fs::create_dir_all(&global_path).context("Failed to create global theme directory")?;
     }
 
     // Runs the app, initializing state using NuhxBoard::new
